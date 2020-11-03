8234e84d83a591e43c679ce4ab9b173e30f4bbeb
cbabalis <babisbabalis@gmail.com>
2020-11-03 17:20:14 +0200

    graphs-in-tabs adds tabs for the data. not yet fully dynamic

--- dash_app/graphs-in-tabs.py
+++ dash_app/graphs-in-tabs.py
@@ -1,26 +1,107 @@
-""" Filters. https://dash.plotly.com/datatable/filtering"""
+"""
+A simple app demonstrating how to dynamically render tab content containing
+dcc.Graph components to ensure graphs get sized correctly. We also show how
+dcc.Store can be used to cache the results of an expensive graph generation
+process so that switching tabs is fast.
+"""
+import time
 
 import dash
-from dash.dependencies import Input, Output
-import dash_table
+import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
+import numpy as np
+import plotly.graph_objs as go
+from dash.dependencies import Input, Output
+import dash_table
 import pandas as pd
-import json
+
 
 df = pd.read_csv('csv_files/1._Goods_Port_throughput.csv')
 
-#df['id'] = df['country']
-#df.set_index('id', inplace=True, drop=False)
-
-app = dash.Dash(__name__)
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.config.suppress_callback_exceptions = True
 
-app.layout = html.Div([
+app.layout = dbc.Container(
+    [
+        dcc.Store(id="store"),
+        html.H1("Dynamically rendered tab content"),
+        html.Hr(),
+        dbc.Button(
+            "Regenerate graphs",
+            color="primary",
+            block=True,
+            id="button",
+            className="mb-3",
+        ),
+        dbc.Tabs(
+            [
+                dbc.Tab(label="Scatter", tab_id="scatter"),
+                dbc.Tab(label="Histograms", tab_id="histogram"),
+                dbc.Tab(label="Goods Port Throughput", tab_id="gpt"),
+            ],
+            id="tabs",
+            active_tab="scatter",
+        ),
+        html.Div(id="tab-content", className="p-4"),
+    ]
+)
 
-    html.H1('1. Goods Port Throughput'),
-    dcc.RadioItems(
+
+@app.callback(
+    Output("tab-content", "children"),
+    [Input("tabs", "active_tab"), Input("store", "data")],
+)
+def render_tab_content(active_tab, data):
+    """
+    This callback takes the 'active_tab' property as input, as well as the
+    stored graphs, and renders the tab content depending on what the value of
+    'active_tab' is.
+    """
+    if active_tab and data is not None:
+        if active_tab == "scatter":
+            return dcc.Graph(figure=data["scatter"])
+        elif active_tab == "histogram":
+            return dbc.Row(
+                [
+                    dbc.Col(dcc.Graph(figure=data["hist_1"]), width=6),
+                    dbc.Col(dcc.Graph(figure=data["hist_2"]), width=6),
+                ]
+            )
+        elif active_tab == "gpt":
+            return filter_query_read_write()
+    return "No tab selected"
+
+
+@app.callback(Output("store", "data"), [Input("button", "n_clicks")])
+def generate_graphs(n):
+    """
+    This callback generates three simple graphs from random data.
+    """
+    if not n:
+        # generate empty graphs when app loads
+        return {k: go.Figure(data=[]) for k in ["scatter", "hist_1", "hist_2"]}
+
+    # simulate expensive graph generation process
+    time.sleep(2)
+
+    # generate 100 multivariate normal samples
+    data = np.random.multivariate_normal([0, 0], [[1, 0.5], [0.5, 1]], 100)
+
+    scatter = go.Figure(
+        data=[go.Scatter(x=data[:, 0], y=data[:, 1], mode="markers")]
+    )
+    hist_1 = go.Figure(data=[go.Histogram(x=data[:, 0])])
+    hist_2 = go.Figure(data=[go.Histogram(x=data[:, 1])])
+
+    # save figures in a dictionary for sending to the dcc.Store
+    return {"scatter": scatter, "hist_1": hist_1, "hist_2": hist_2}
+
+
+def filter_query_read_write():
+    fqrr = html.Div ([
+        dcc.RadioItems(
         id='filter-query-read-write',
         options=[
             {'label': 'Read filter_query', 'value': 'read'},
@@ -62,8 +143,8 @@ app.layout = html.Div([
     html.Div(id='datatable-interactivity-container'),
     html.Hr(),
     html.Div(id='datatable-query-structure', style={'whitespace': 'pre'})
-])
-
+    ])
+    return fqrr
 
 @app.callback(
     [Output('filter-query-input', 'style'),
@@ -128,6 +209,7 @@ def update_styles(selected_columns):
     } for i in selected_columns]
 
 
+
 @app.callback(
     Output('datatable-interactivity-container', "children"),
     [Input('datatable-advanced-filtering', "derived_virtual_data"),
@@ -183,6 +265,5 @@ def update_graphs(rows, derived_virtual_selected_rows):
     return all_vs_all
 
 
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+if __name__ == "__main__":
+    app.run_server(debug=True, port=8888)
